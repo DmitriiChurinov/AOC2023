6d846b5cbe5474eff0f5298c4828843c3f663eef
Дмитрий Чуринов <41968885+DmitriiChurinov@users.noreply.github.com>
2023-12-01 19:48:22 +0300

    day1: add example tests

--- day1.cpp
+++ day1.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <cassert>
+#include <sstream>
 
 int CheckWithDigitStrings(const std::string& str, const std::vector<std::string>& digit_string, int index) {
     int size = static_cast<int>(digit_string.size());
@@ -97,12 +98,31 @@ int GetNumberWithReplaceString(const std::string& str, const std::vector<std::st
     return first * 10 + second;
 }
 
+bool SumTest(const std::vector<std::string>& test_data, const std::vector<std::string>& digit_strings, bool with_string_digit, int answer) {
+    int sum = 0;
+    for (auto str : test_data) {
+        int string_number = GetNumber(str, digit_strings, with_string_digit);
+        int string_number_with_replace = GetNumberWithReplaceString(str, digit_strings, with_string_digit);
+        assert(string_number == string_number_with_replace);
+        sum += string_number;
+    }
+
+    return sum == answer;
+}
+bool ValidateTest(const std::vector<std::string>& digit_strings) {
+    const std::vector<std::string>& example1 = {"1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"};
+    const std::vector<std::string>& example2 = {"two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"};
+    return SumTest(example1, digit_strings, false, 142)
+        && SumTest(example2, digit_strings, true, 281);
+}
+
 int main() {
-    std::ifstream fio;
-    fio.open("1.txt");
-    int task1_1_sum(0), task1_2_sum(0);
     std::vector<std::string> digit_string = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+    assert(ValidateTest(digit_string));
+    std::ifstream fio;
+    fio.open("1_2_test");
+    int task1_1_sum(0), task1_2_sum(0);
     if (fio.is_open()) {
         std::string str;
 
@@ -116,9 +136,10 @@ int main() {
             assert(string_number_with_digit_string == string_number_with_digit_string_with_replace);
             task1_2_sum += string_number_with_digit_string;
         }
+        fio.close();
     }
 
-    fio.close();
+
     std::cout << "1_1: " << task1_1_sum << std::endl;
     std::cout << "1_2: " << task1_2_sum << std::endl;
 
